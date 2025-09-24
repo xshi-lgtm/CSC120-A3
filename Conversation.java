11811970f3482ae956a285ba62403dcbd10a3177
tangtangyuhan <atang05@smith.edu>
2025-09-23 21:17:11 -0400

    finished

--- Conversation.java
+++ Conversation.java
@@ -5,18 +5,22 @@ import java.util.Random;
 
 class Conversation implements ConversationRequirements {
 
-  // Attributes 
-  Scanner scanner;
-  List <String> transcript;
-  String [] replies;
-  String [] detectedWords;
-  String [] changeWord;
+  // Attributes
+  Scanner scanner;   // the conversation should be able to reads user input from console
+  List <String> transcript; // creates arraylist to store chat history
+  String [] replies; // list of canned replies
+  String [] detectedWords; // list of target words
+  String [] changeWord; // corresponding list of changed words with target words
 
   /**
-   * Constructor 
+   * Creates a new object of conversation (a constructor)
+   * @param history the conversation history
+   * @param fixed_replies arraylist of canned responses
+   * @param fromwords words to be detected from user input
+   * @param towords changed list of words that corresponds with the detected words
    */
   Conversation(List<String> history, String[] fixed_replies, String[] fromwords, String[] towords) {
-    this.scanner = new Scanner(System.in);
+    this.scanner = new Scanner(System.in); // creates new scanner for user input
     this.transcript = history;
     this.replies = fixed_replies;
     this.detectedWords = fromwords;
@@ -27,31 +31,35 @@ class Conversation implements ConversationRequirements {
    * Starts and runs the conversation with the user
    */
   public void chat() {
-    System.out.println("Hello! Welcome to Amy and Cola's chat bot!");
-    System.out.println("How many rounds would you like to talk to us :)");
+      System.out.println("Hello! Welcome to Amy and Cola's chat bot!");//the greeting message
+      System.out.println("How many rounds would you like to talk to us :)");//ask how many rounds to get int n
 
-    int n = scanner.nextInt();
-    scanner.nextLine();
-    System.out.println("What's on your mind?");
+      int n = scanner.nextInt();//get user's input of how many rounds they would like to have
+      scanner.nextLine();
+      System.out.println("What's on your mind?");//To start the conversation
+      transcript.add("Bot: "+ "What's on your mind?");
 
-    for (int i = 0; i < n; i++){
-      String user_input = scanner.nextLine();
+      for (int i = 0; i < n; i++){//for loop for the conversation
+        String user_input = scanner.nextLine();//user's input
 
-      transcript.add("User: "+ user_input);
-      String bot_respond = respond(user_input);
-      System.out.println(bot_respond);
-      transcript.add("Bot:"+ bot_respond);
-    }
-    System.out.println("See ya!");
+        transcript.add("User: "+ user_input);//add user's input to. the transcript
+        String bot_respond = respond(user_input);//generate the respond by using respond method
+        System.out.println(bot_respond + "?");//print out the respond
+        transcript.add("Bot:"+ bot_respond + "?");//add the chatbox's respond to the transcript
+      }
+      System.out.println("See ya!");
+      transcript.add("Bot: "+ "See ya!");
   }
 
 
+
+
   /**
-   * Prints transcript of conversation
+   * Prints transcript of conversation line by line
    */
   public void printTranscript() {
-    System.out.println("\nTranscript:");
-    for (int i = 0; i < transcript.size(); i++){
+    System.out.println("\nTranscript:"); // prints a header before the transcript
+    for (int i = 0; i < transcript.size(); i++){ // use for loop to iterate over the list of string of transcript
       System.out.println(transcript.get(i));
     }
   }
@@ -63,38 +71,42 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    Boolean replaced = false;
-    String[] respond_list =  inputString.split(" ");
+    Boolean replaced = false; // first set determinant boolean expression to false
+    String no_punc = inputString.replaceAll("[^a-zA-Z0-9\\s‘’'“”]", ""); // remove all non-letter characters except '',"",' from the string
+    String[] respond_list =  no_punc.split(" "); // split the string into list of separated words by " "
 
-    for (int j = 0; j < respond_list.length; j++) {
-      String respond_words = respond_list[j].toLowerCase();
-      for (int i = 0; i < detectedWords.length; i++) {
-        String from = detectedWords[i].toLowerCase();
+
+    for (int j = 0; j < respond_list.length; j++) { // iterate over list of words converted from user input string 
+      String respond_words = respond_list[j].toLowerCase(); // convert all letters to lower case to make detection more accurate
+      for (int i = 0; i < detectedWords.length; i++) { //iterates over the detected words
+        String from = detectedWords[i].toLowerCase(); 
           if (respond_words.equals(from)) {
-              respond_list[j] = changeWord[i];
-              replaced = true;
+              respond_list[j] = changeWord[i]; // change words if detected words is exactly the same as respond words
+              replaced = true; // set the determinant boolean to true if words are replaced
               }
           }
       }
 
     if (replaced){
-      String result = String.join(" ", respond_list);
+      String result = String.join(" ", respond_list); //join the respond_list into a string
       return result;
     }else{
-        Random rand = new Random();
+        Random rand = new Random(); //get a random number for the index of the replies list
         int index = rand.nextInt(replies.length);
-        return (replies[index]);
+        return (replies[index]); //return a random fixed reply
     }
   }
 
-
+  /**
+   * Creates a new object named myConversation, runs the chat method and prints the transcript
+   */
   public static void main(String[] arguments) {
-    List<String> history = new ArrayList<>();
-    String[] fixed_replies = new String[]{"Mmm-hm", "That's great", "Interesting", "I see...", "I want know more about this"};
-    String[] fromwords = new String[]{"I","me", "am", "you", "my", "your", "are"};
-    String[] towords   = new String[]{"you","you", "are", "me","your", "my", "is"};
-    Conversation myConversation = new Conversation(history, fixed_replies, fromwords, towords);
-    myConversation.chat();
-    myConversation.printTranscript();
+    List<String> history = new ArrayList<>(); // creates an empty array list for new chat history to store
+    String[] fixed_replies = new String[]{"Mmm-hm", "That's great", "Interesting", "I see...", "I want know more about this"}; // creates the list of canned response
+    String[] fromwords = new String[]{"I","me", "am", "you", "my", "your", "are", "I'm", "You're"}; // creates the list of key words
+    String[] towords   = new String[]{"you","you", "are", "I","your", "my", "is", "You're", "I'm"}; // creates the list of changed words
+    Conversation myConversation = new Conversation(history, fixed_replies, fromwords, towords); // creates a new object named myConversation
+    myConversation.chat(); // uses the method chat to the object myConversation, performing the functions of chatbox
+    myConversation.printTranscript(); // uses the method printTranscript to the object myConversation, making it print out the chat history
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Jessie Wang, Selina Fang, Ella Wang
+
 - Which session(s) of TA / office hours did you attend?
+Sunday 7:00 - 9:00 p.m.
+
 - What are your initial impressions of Java? 
+The way we organize our codes (in classes) and the syntax is very different from python. Also, the syntax rules are very strict, which we are forced to write the type for each variable, and this also helps the coding processes greatly.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+There are a lot of similarities in programming logic in Python and Java, specifically, how we managed to turn user input into a list and then use for loop to check whether the list contains targeted words. This is where we can turn what we've learned in python into programming with Java. However, there are more differences between Python and Java coding where we need to search online for a lot of times: although the logic is generally the same, the syntax differ greatly. For example, we cannot directly apply how we used to write for loops for python into Java coding. Also, we find it hard to switch from the logic for procedural programming to object-oriented programming. Therefore, in the future, we will put our focus into learning more Java syntax for string methods, list methods, and dictionary methods, for example, and also pay more effort into turning our thinking processes into a object-oriented way.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+We find it comparatively easy to write the attributes and constructors, and also the chat method, which is the large running structure of the chat box. However, we faced a lot of difficuilties when writing the method of respond, specifically, how to mirror the message and replace the word of "you", "I", etc. We first tried to use the string method .contain("abc"), but found out that this method does a lot of misidentification such as replacing the "I" in "Inside". Then, we thought about how we strip strings into list and then use for loop to do the comparison one by one. Then, we get to accurately identify target words without  mistankenly replacing the word, and also avoid repetitive replacing targeted words again and again in for loop. 
